8627a6822f01776c08118faefbb331aae11411f8
NguyenQuang203 <nguyenthienquang20022003@gmail.com>
2024-09-24 10:30:14 +0700

    'Start_Buiding'

---
+++ tour-service/src/test/java/com/booking/tour_service/TourServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.tour_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
