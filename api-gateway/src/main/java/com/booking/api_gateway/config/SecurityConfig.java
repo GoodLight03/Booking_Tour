e515122181854b13d403e2484d90cd9e0aab033f
NguyenQuang203 <nguyenthienquang20022003@gmail.com>
2024-10-11 12:44:56 +0700

    Start Outh02

---
+++ api-gateway/src/main/java/com/booking/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.booking.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity.authorizeExchange(exchanges -> exchanges.pathMatchers(HttpMethod.GET).permitAll()
+                        .pathMatchers("/api/locations/**").hasRole("ACCOUNTS")
+                        .pathMatchers("/api/tour/**").authenticated()
+                        .pathMatchers("/api/booking/**").authenticated())
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
+                        .jwt(
+                                Customizer.withDefaults()
+                               // jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())
+                        )
+                );
+        serverHttpSecurity.csrf(csrfSpec -> csrfSpec.disable());
+        return serverHttpSecurity.build();
+    }
+}
